1d6119a5f6595b94a20e915978bc271b36a5d393
Ivan Szebenszki <szib77@gmail.com>
2019-03-07 22:00:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  ret = []
+  ret << 'Fizz' if num % 3 == 0
+  ret << 'Buzz' if num % 5 == 0
+  return ret.length == 0 ? nil : ret.join()
+end
